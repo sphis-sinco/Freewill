9f46644d6da8fcfc9b29f430111acf0028ee3491
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-20 08:49:52 -0500

    window title setting

--- source/Global.hx
+++ source/Global.hx
@@ -4,7 +4,7 @@ import openfl.Lib;
 
 class Global
 {
-        public static var lyricsObject:Lyrics = new Lyrics();
+	public static var lyricsObject:Lyrics = new Lyrics();
 
 	public static function resizeWindow(width:Int = 820, height:Int = 720) // yoinked from rewrite round 2 LOL
 	{
@@ -14,5 +14,23 @@ class Global
 		var resolutionY = Math.ceil(Lib.current.stage.window.display.currentMode.height * Lib.current.stage.window.scale);
 		Lib.application.window.x = Std.int((resolutionX - Lib.application.window.width) / 2);
 		Lib.application.window.y = Std.int((resolutionY - Lib.application.window.height) / 2);
+	}
+	public static var defaultWindowTitle:String = "Freewill";
+
+	public static function setWindowTitle(value:String):Void
+	{
+		if (value != "reset")
+		{
+			Lib.application.window.title = value;
+		}
+		else
+		{
+			resetWindowTitle();
+		}
+	}
+
+	public static function resetWindowTitle():Void
+	{
+		Lib.application.window.title = defaultWindowTitle;
 	}
 }
